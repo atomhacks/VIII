15f3a86bbb4412fef34856a6f75d6c2b79ff718e
warren yun <xxwarrenhdxx@gmail.com>
2021-12-05 02:52:05 -0500

    Navbar links

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,15 @@
 import Image from 'next/image';
 import navStyles from '@styles/components/Nav.module.css';
+import Link from 'next/link';
+
+const routes = [
+    { name: 'Home', href: '/#home', current: false },
+    { name: 'About', href: '/#about', current: false },
+    { name: 'Sponsors', href: '/#sponsors', current: false },
+    { name: 'Schedule', href: '/#schedule', current: false },
+    { name: 'Team', href: '/#team', current: false },
+    { name: 'FAQ', href: '/#faq', current: false },
+]
 
 const Nav = () => {
     return (
@@ -11,7 +21,18 @@ const Nav = () => {
                 width={50}
             >
             </Image>
-            <h2 className={navStyles.home}>AtomHacks</h2>
+            <h2 className={navStyles.navItem}>AtomHacks</h2>
+            <div className={navStyles.right}>
+                {routes.map((route, i) => (
+                    <Link
+                        href={route.href}
+                        key={i}
+                        passHref
+                    >
+                        <h2 className={navStyles.navItem}>{route.name}</h2>
+                    </Link>
+                ))}
+            </div>
         </nav>
     );
 }
