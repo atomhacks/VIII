4693198d88f6914092f3bf7f8611510d7b6cfe5a
warren yun <xxwarrenhdxx@gmail.com>
2021-12-05 01:25:27 -0500

    Create next.js project

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Atomhacks</title>
+      </Head>
+
+      <div className={styles.container}>
+        <h1>AHHHHHHHHHHHHHHHHHHHHHHHHHHHH</h1>
+      </div>
+    </>
+  )
+}
