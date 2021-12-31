5f7b60476ec23726b282e934d66b6212a31cd329
bdey05 <73259986+bdey05@users.noreply.github.com>
2021-12-30 21:17:42 -0500

    Add files via upload

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,12 +1,38 @@
 import galleryStyles from '@styles/components/gallery.module.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "swiper/css";
+import "swiper/css/navigation"
+import SwiperCore, {
+    Navigation
+} from 'swiper';
+import Image from "next/image";
+
+SwiperCore.use([Navigation]);
 
 export default function Gallery() {
     return (
         <div className={galleryStyles.galleryDiv}>
-                <h1 className={`title ${galleryStyles.titleshadow}`}>Gallery</h1>
-                <div className={galleryStyles.mainBody}>
-                    <h2>Test</h2>
-                </div>
+            <h1 className={`title ${galleryStyles.titleshadow}`}>Gallery</h1>
+            <div className={galleryStyles.mainBody}>
+                <h2>Test</h2>
+                <Swiper navigation={true} loop={true}>
+                    <SwiperSlide>
+                        <h2>Slide 1</h2>
+                         <Image src="/images/logos/logo.png" width={500} height={500}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <h2>Slide 2</h2>
+                         <Image src="/images/logos/logo.png" width={500} height={500}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <h2>Slide 3</h2>
+                         <Image src="/images/logos/logo.png" width={500} height={500}/>
+                    </SwiperSlide>
+
+                </Swiper>
+            </div>
+
+
         </div>
     );
 }
