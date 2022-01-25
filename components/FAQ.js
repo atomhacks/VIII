abef5cab677a4b33c1f68fcd4692afbdabbf867e
warren yun <wyun2006@gmail.com>
2022-01-24 21:10:07 -0500

    faq grid

--- components/FAQ.js
+++ components/FAQ.js
@@ -3,10 +3,24 @@ import faqStyles from '@styles/sections/FAQ.module.css';
 const FAQ = () => {
     return (
         <div className={faqStyles.FAQdiv}>
-            <div />
             <div>
                 <h1 className={faqStyles.title}>faq</h1>
             </div>
+            <div className={faqStyles.questionsDiv}>
+                <div>
+                    <h1 className={faqStyles.question}>TEST</h1>
+                </div>
+                <div>
+                    <h1 className={faqStyles.question}>TEST</h1>
+                </div>
+                <div>
+                    <h1 className={faqStyles.question}>TEST</h1>
+                </div>
+                <div>
+                    <h1 className={faqStyles.question}>TEST</h1>
+                </div>
+            </div>
+
         </div>
     );
 }
