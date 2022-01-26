9b875e554de9992c7b6eebaf7d24cdee3c12be67
warren yun <wyun2006@gmail.com>
2022-01-26 14:00:01 -0500

    Set up team page styling

--- components/sections/Team.js
+++ components/sections/Team.js
@@ -1,9 +1,21 @@
 import teamStyles from '@styles/sections/Team.module.css';
 
+const programming = [
+    {
+        image: '',
+        name: ''
+    }
+]
+
 const Team = () => {
     return (
         <div className={teamStyles.teamDiv}>
-            <h1>b</h1>
+            <h1 className={teamStyles.title}>Team</h1>
+            <div className={teamStyles.team}>
+                <div className={teamStyles.department}>
+                
+                </div>
+            </div>
         </div>
     );
 }
