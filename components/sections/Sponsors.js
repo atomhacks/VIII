8aad61e952fccbbb04b42b02bc9ae2a41d4cab80
warren yun <wyun2006@gmail.com>
2022-01-28 13:46:38 -0500

    color variables

--- components/sections/Sponsors.js
+++ components/sections/Sponsors.js
@@ -4,8 +4,9 @@ const Sponsor = () => {
   return (
     <div className={sponsorStyles.sponsorDiv}>
       <h1 className={sponsorStyles.title}>Sponsors</h1>
-      <b>We are farmers</b>
-      <h2>BUM BUDA BUM BUM BUM BUM BUM </h2>
+      <div className={sponsorStyles.logos}>
+
+      </div>
     </div>
   );
 };
