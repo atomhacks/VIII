3de8208718336e78d70eb87490326011191342a3
bdey05 <73259986+bdey05@users.noreply.github.com>
2022-02-01 22:44:18 -0500

    Add files via upload

--- pages/gallery.js
+++ pages/gallery.js
@@ -4,11 +4,17 @@ import "swiper/css";
 import "swiper/css/navigation";
 import SwiperCore, { EffectFade, Navigation } from "swiper";
 import Image from "next/image";
+import useEmblaCarousel from "embla-carousel-react";
+import ImageSlider from "@components/Slider";
+import { SliderData } from "@components/SliderData";
 
 SwiperCore.use([EffectFade, Navigation]);
 //Testing Swiper Slides
 
+
+
 export default function Gallery() {
+  
   return (
     <div className={galleryStyles.galleryDiv}>
       <h1 className={`title ${galleryStyles.titleshadow}`}>Gallery</h1>
@@ -16,79 +22,32 @@ export default function Gallery() {
         <div className={galleryStyles.galleryContainer}>
           <div className={galleryStyles.leftSliderContainer}>
             <div className={galleryStyles.date}>
-              <h2>2022</h2>
+              <h2>2021</h2>
             </div>
-
-            <div className={galleryStyles.slider}>
-              <Swiper loop={true} navigation={true}>
-                <SwiperSlide>
-                  <h2>Slide 1</h2>
-                  <Image
-                    src="/images/test1.jpeg"
-                    width={700}
-                    height={400}
-                    alt="img 1"
-                  />
-                </SwiperSlide>
-                <SwiperSlide>
-                  <h2>Slide 2</h2>
-                  <Image
-                    src="/images/logos/logo.png"
-                    width={500}
-                    height={500}
-                    alt="img 2"
-                  />
-                </SwiperSlide>
-                <SwiperSlide>
-                  <h2>Slide 3</h2>
-                  <Image
-                    src="/images/logos/logo.png"
-                    width={500}
-                    height={500}
-                    alt="img 3"
-                  />
-                </SwiperSlide>
-              </Swiper>
+            <div className={galleryStyles.sliderCon}>
+            <ImageSlider slides={SliderData}/>
             </div>
           </div>
           <div className={galleryStyles.leftSliderContainer}>
-            <div className={galleryStyles.slider}>
-              <Swiper loop={true} navigation={true}>
-                <SwiperSlide>
-                  <h2>Slide 1</h2>
-                  <Image
-                    src="/images/test1.jpeg"
-                    width={700}
-                    height={400}
-                    alt="img 1"
-                  />
-                </SwiperSlide>
-                <SwiperSlide>
-                  <h2>Slide 2</h2>
-                  <Image
-                    src="/images/logos/logo.png"
-                    width={500}
-                    height={500}
-                    alt="img 2"
-                  />
-                </SwiperSlide>
-                <SwiperSlide>
-                  <h2>Slide 3</h2>
-                  <Image
-                    src="/images/logos/logo.png"
-                    width={500}
-                    height={500}
-                    alt="img 3"
-                  />
-                </SwiperSlide>
-              </Swiper>
+            <div className={galleryStyles.sliderCon}>
+            <ImageSlider slides={SliderData}/>
             </div>
             <div className={galleryStyles.date}>
-              <h2>2021</h2>
+              <h2>2019</h2>
             </div>
           </div>
+    
+
         </div>
+
+      
+        
       </div>
+
+      
+
+     
+
     </div>
   );
 }
