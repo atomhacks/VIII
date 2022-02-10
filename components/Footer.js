1dada0e594afe9bd3df17878467b2347f5b9028c
warren yun <wyun2006@gmail.com>
2022-02-10 15:12:19 -0500

    Init footer

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import footerStyles from '@styles/components/Footer.module.css';
+
+const Footer = () => {
+    return (
+        <div className={footerStyles.container}>
+
+        </div>
+    );
+}
+
+export default Footer;
