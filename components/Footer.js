43563db9dbf39dfc79ce8fef79ae08304a7462d1
warren yun <wyun2006@gmail.com>
2022-02-19 07:46:42 -0500

    vercel very cool

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,44 @@
 import Image from "next/image";
-
 import footerStyles from "@styles/components/Footer.module.css";
+
+const socials = [
+  {
+    image: "/images/footer-icons/envelope.svg",
+    link: "",
+    alt: "email",
+  },
+  {
+    image: "/images/footer-icons/github.svg",
+    link: "",
+    alt: "github",
+  },
+  {
+    image: "/images/footer-icons/instagram.svg",
+    link: "",
+    alt: "insta",
+  },
+];
 
 const Footer = () => {
   return (
     <div className={footerStyles.container}>
       <h1 className={footerStyles.title}>atomhacks</h1>
       <div className={footerStyles.social}></div>
-      <Image
-        src="/images/footer-icons/powered-by-vercel.svg"
-        width={200}
-        height={100}
-        alt="Powered by vercel"
-      />
-      <p>© Copyright 2021, Bronx High School of Science AtomHacks.</p>
+
+      <a
+        href="https://vercel.com/dashboard?utm_source=atomhacks-website&utm_campaign=oss"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image
+          src="/images/footer-icons/powered-by-vercel.svg"
+          width={200}
+          height={100}
+          alt="Powered by vercel"
+        />
+      </a>
+
+      <p>© Copyright 2022, Bronx High School of Science AtomHacks.</p>
     </div>
   );
 };
