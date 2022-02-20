0fb37e3193925723b5dc180845bc0d13dcbb74bc
warren yun <wyun2006@gmail.com>
2022-02-20 13:39:22 -0500

    Add footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,18 +4,23 @@ import footerStyles from "@styles/components/Footer.module.css";
 const socials = [
   {
     image: "/images/footer-icons/envelope.svg",
-    link: "",
+    link: "mailto:atomhacks@bxscience.edu",
     alt: "email",
   },
   {
     image: "/images/footer-icons/github.svg",
-    link: "",
+    link: "https://github.com/atomhacks",
     alt: "github",
   },
   {
     image: "/images/footer-icons/instagram.svg",
-    link: "",
+    link: "https://www.instagram.com/bxsciatomhacks/",
     alt: "insta",
+  },
+  {
+    image: "/images/footer-icons/facebook.svg",
+    link: "https://www.facebook.com/BxSciHackathoners",
+    alt: "facebook",
   },
 ];
 
@@ -37,6 +42,21 @@ const Footer = () => {
           alt="Powered by vercel"
         />
       </a>
+
+      <div className={footerStyles.iconDiv}>
+        {socials.map((social, i) => (
+          <div key={i} className={footerStyles.icon}>
+            <a href={social.link}>
+              <Image
+                src={social.image}
+                alt={social.alt}
+                width={30}
+                height={30}
+              />
+            </a>
+          </div>
+        ))}
+      </div>
 
       <p>© Copyright 2022, Bronx High School of Science AtomHacks.</p>
     </div>
