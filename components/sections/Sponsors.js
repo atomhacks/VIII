792a86234be637ab5b9a1fc5b8d51ffeaeda131e
warren yun <wyun2006@gmail.com>
2022-02-26 17:09:52 -0500

    add alumni foundation

--- components/sections/Sponsors.js
+++ components/sections/Sponsors.js
@@ -5,27 +5,32 @@ const Sponsors = [
   {
     name: "digital ocean",
     path: "digital_ocean.svg",
-    link: "https://www.digitalocean.com/"
+    link: "https://www.digitalocean.com/",
   },
   {
     name: "wolfram language",
     path: "wolfram.png",
-    link: "https://www.wolfram.com/language/"
+    link: "https://www.wolfram.com/language/",
   },
   {
     name: "sticker giant",
     path: "sticker_giant.png",
-    link: "https://www.stickergiant.com/event-coupons?utm_source=website&utm_medium=event"
+    link: "https://www.stickergiant.com/event-coupons?utm_source=website&utm_medium=event",
   },
   {
     name: "sticker mule",
     path: "sticker_mule.png",
-    link: "https://www.stickermule.com/"
+    link: "https://www.stickermule.com/",
   },
   {
     name: "vercel",
     path: "vercel.png",
-    link: "https://vercel.com/"
+    link: "https://vercel.com/",
+  },
+  {
+    name: "alumni foundation",
+    path: "alumni_foundation.png",
+    link: "",
   },
 ];
 
@@ -33,9 +38,31 @@ const Sponsor = () => {
   return (
     <div className={sponsorStyles.sponsorDiv}>
       <h1 className={sponsorStyles.title}>Sponsors</h1>
-      <p style={{'textAlign': 'center', 'fontSize': '200%', 'marginTop': '6rem', 'fontWeight': '500'}}>Thanks to our wonderful sponsors for making AtomHacks possible!</p>
+      <p
+        style={{
+          textAlign: "center",
+          fontSize: "200%",
+          marginTop: "6rem",
+          fontWeight: "500",
+        }}
+      >
+        Thanks to our wonderful sponsors for making AtomHacks possible!
+      </p>
 
       <div className={sponsorStyles.logos}>
+        <a
+          href="https://smallplanet.com/"
+          target="_blank"
+          rel="noreferrer"
+          className={sponsorStyles.logo}
+        >
+          <Image
+            src="/images/sponsors/small_planet.png"
+            alt="logo"
+            width={200}
+            height={250}
+          />
+        </a>
         {Sponsors.map((spons, i) => (
           <a href={spons.link} key={i} target="_blank" rel="noreferrer">
             <img
@@ -45,14 +72,6 @@ const Sponsor = () => {
             />
           </a>
         ))}
-        <a href="https://smallplanet.com/" target="_blank" rel="noreferrer" className={sponsorStyles.logo}>
-          <Image
-            src="/images/sponsors/small_planet.png"
-            alt="logo"
-            width={200}
-            height={250}
-          />
-        </a>
       </div>
     </div>
   );
