b3468ac13965bd2bd204d53a0aac9eb1a48705ba
bdey05 <73259986+bdey05@users.noreply.github.com>
2022-03-29 22:09:08 -0400

    Add files via upload

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,54 +1,56 @@
 import galleryStyles from "@styles/components/gallery.module.css";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import SwiperCore, { EffectFade, Navigation } from "swiper";
+
 import Image from "next/image";
 
-// import useEmblaCarousel from "embla-carousel-react/components";
-import ImageSlider from "@components/Slider";
-import { SliderData } from "@components/SliderData";
-
-SwiperCore.use([EffectFade, Navigation]);
-//Testing Swiper Slides
-
-
-
 export default function Gallery() {
-  
+
+  const array1 = []
+  for (let i = 1; i <= 12; i++) {
+    array1.push(`/images/gallery/img${i}.png`)
+  }
+
+  const array2 = []
+  for (let i = 13; i <= 22; i++){
+    array2.push(`/images/gallery/img${i}.JPG`)
+  }
+
   return (
     <div className={galleryStyles.galleryDiv}>
       <h1 className={`title ${galleryStyles.titleshadow}`}>Gallery</h1>
       <div className={galleryStyles.mainBody}>
-        <div className={galleryStyles.galleryContainer}>
-          <div className={galleryStyles.leftSliderContainer}>
-            <div className={galleryStyles.date}>
-              <h2>2021</h2>
-            </div>
-            <div className={galleryStyles.sliderCon}>
-            <ImageSlider slides={SliderData}/>
-            </div>
+        <div className={galleryStyles.gridContainer}>
+          <h2 className={galleryStyles.gridTitle}>2021</h2>
+          <p className={galleryStyles.gridCaption}>This was the first year that the hackathon went online</p>
+          <div className={galleryStyles.gridImages}>
+            {array1.map((image, i) => (
+                <div className={galleryStyles.gridImage}>
+                  <Image
+                  src = {image}
+                  alt = "img"
+                  layout="fill"
+                  objectFit="cover">
+                  </Image>
+                </div>
+            ))}
           </div>
-          <div className={galleryStyles.leftSliderContainer}>
-            <div className={galleryStyles.sliderCon}>
-            <ImageSlider slides={SliderData}/>
-            </div>
-            <div className={galleryStyles.date}>
-              <h2>2019</h2>
-            </div>
-          </div>
-    
-
         </div>
-
-      
-        
+        <div className={galleryStyles.gridContainer}>
+          <h2 className={galleryStyles.gridTitle}>2019</h2>
+          <p className={galleryStyles.gridCaption}>A great year for the hackathon</p>
+          <div className={galleryStyles.gridImages}>
+            {array2.map((image, i) => (
+                <div className={galleryStyles.gridImage}>
+                  <Image
+                  src = {image}
+                  alt = "img"
+                  layout="fill"
+                  objectFit="cover">
+                  </Image>
+                </div>
+            ))}
+          </div>
+        </div>
       </div>
-
-      
-
-     
-
     </div>
   );
 }
